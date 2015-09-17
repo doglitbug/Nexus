7ee664cb4bf76b22e4ef9e50a4fe3a8f4170ca26
doglitbug <dickaj1@student.op.ac.nz>
2015-09-17 12:03:34 +1200

    Forgot to save

--- Nexus/board.cpp
+++ Nexus/board.cpp
@@ -10,10 +10,18 @@ void board::resetBoard(){
 }
 
 int board::getCell(int pX, int pY){
-	//TODO: Check bounds of pX and pY?
+	//Check bounds of pX and pY and retunr -1 if outside of the board
 	if (pX<0 || pX>BOARD_SIZE || pY<0 || pY>BOARD_SIZE){
 		return -1;
 	} else {
 		return mBoard[pX][pY];
+	}
+}
+
+void board::setCell(int pX, int pY, int contents){
+	if (pX<0 || pX>BOARD_SIZE || pY<0 || pY>BOARD_SIZE){
+		//TODO out of bounds
+	} else {
+		mBoard[pX][pY]=contents;
 	}
 }
